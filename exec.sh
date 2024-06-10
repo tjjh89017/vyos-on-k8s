2d53b838ad42c0c334fee9c0062273345aef3322
Date Huang <date@vyos.io>
2024-06-10 23:50:24 +0800

    f
    Signed-off-by: Date Huang <date@vyos.io>

--- exec.sh
+++ exec.sh
@@ -1,9 +1,13 @@
 source /opt/vyatta/etc/functions/script-template
 configure
-load /opt/vyatta/etc/config.boot.default
-commit
+if [ -s /config/config.temp ]
+then
 load /config/config.temp
 commit
+else
+load /opt/vyatta/etc/config.boot.default
+commit
+fi
 source /config/config.command
 commit
 exit
